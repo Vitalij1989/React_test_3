ff95ca4295a581c58b41fecd5ef8282116967466
Vitalij1989 <108751440+Vitalij1989@users.noreply.github.com>
2023-02-26 11:30:30 +0200

    start

--- start-files/src/views/Home.jsx
+++ start-files/src/views/Home.jsx
@@ -1,12 +1,18 @@
-import { CocktailsList } from '../components/CocktailsList';
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
+import { CocktailsList } from "../components/CocktailsList";
+import { getTrendingCocktails } from ".././api/cocktail-service";
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
 
 export const Home = () => {
+  const [cocktails, setCocktails] = useState([]);
+  useEffect(() => {
+    getTrendingCocktails().then((respons) => console.log(respons));
+  }, []);
+
   return (
     <>
       <Section>
-        <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
+        <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
           Trending cocktails
         </h1>
 
